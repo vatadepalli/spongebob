52cc07675a3b31b161a1241bbd3c89759b0b4d55
Vijaya Aditya <vatadepalli@hotmail.com>
2019-07-31 15:13:30 +0530

    Repaired Hot Reload. - Now its really hot

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from "../node_modules/react";
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
 import "./App.css";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Content from "./components/Content";
-// import { View, TitleBar } from "react-desktop/macOs";
 
-export default class App extends Component {
+class App extends Component {
   constructor() {
     super();
     this.state = { isFullscreen: false };
@@ -15,17 +15,6 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <TitleBar
-          title="untitled text 5"
-          controls
-          isFullscreen={this.state.isFullscreen}
-          onCloseClick={() => console.log("Close window")}
-          onMinimizeClick={() => console.log("Minimize window")}
-          onMaximizeClick={() => console.log("Mazimize window")}
-          onResizeClick={() =>
-            this.setState({ isFullscreen: !this.state.isFullscreen })
-          }
-        />*/}
         <div className="Header">
           <Header />
         </div>
@@ -39,3 +28,5 @@ export default class App extends Component {
     );
   }
 }
+
+export default hot(module)(App);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
@@ -22,7 +23,8 @@ module.exports = {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader"
+            loader: "html-loader",
+            options: { minimize: true }
           }
         ]
       },
@@ -34,6 +36,14 @@ module.exports = {
           __dirname + "/node_modules/codemirror/theme/monokai.css",
           __dirname + "/node_modules/codemirror/lib/codemirror.css"
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   },
@@ -41,6 +51,20 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     })
-  ]
+  ],
+  devServer: {
+    port: 3001
+  },
+  devServer: {
+    stats: {
+      children: false, // Hide children information
+      maxModules: 0 // Set the maximum number of modules to be shown
+    },
+    port: 3001
+  }
 };
